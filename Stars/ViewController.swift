440979e031e344c2896a44e7995555f6458a575e
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2019-10-03 18:36:42 -0700

    Initial Commit

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Dimitri Bouniol Lambda on 10/3/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
